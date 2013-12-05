ca21382f26dd71a4d169190a08b6b94e35f986ed
jheytompkins <jhey.tompkins@gmail.com>
2013-12-05 11:54:48 +0000

    added disabled flag to draggable so can disable it dynamically if waiting for something for example

--- draggable.js
+++ draggable.js
@@ -210,7 +210,8 @@ function draggable(element, options) {
 		contained: false,
 		pens: false,
 		vertical: true,
-		horizontal: true
+		horizontal: true,
+		disabled: false
 	};
 	var extend = function (a, b) {
 		for(var key in b) {
@@ -225,6 +226,7 @@ function draggable(element, options) {
 	this._roam = (this._options.roam  !== undefined) ? this._options.roam : true;
 	this._contained = (this._options.contained !== undefined) ? this._options.contained : false;
 	this._pens = this._options.pens;
+	this._disabled = (this._options.disabled !== undefined) ? this._options.disabled : true;
 	this._vertical = (this._options.vertical !== undefined) ? this._options.vertical : true;
 	this._horizontal = (this._options.horizontal !== undefined) ? this._options.horizontal : true;
 	this._ghosting = (this._options.ghosting !== undefined) ? this._options.ghosting : false;
@@ -233,6 +235,11 @@ function draggable(element, options) {
 draggable.prototype.setPens = function (pens) {
 	if (pens) {
 		this._pens = pens;
+	}
+}
+draggable.prototype.setDisabled = function (disabled) {
+	if (disabled !== undefined) {
+		this._disabled = disabled;
 	}
 }
 draggable.prototype.setContained = function (contained) {
@@ -294,56 +301,64 @@ draggable.prototype._create = function () {
 			draggable._parent.addEventListener('dblclick', endDrag);
 		},
 		endDrag = function () {
-			if (ghost !== undefined) {
-				ghost.remove();
-			}
-			draggable._parent.removeEventListener('mousemove', drag, true);
-			if (draggable._pens && draggable._pens.length > 0) {
-				var penned = false,
-					currentPen = draggable.element.parentNode,
-					isAPen = function (element) {
-						for (var i = 0; i <= draggable._pens.length - 1; i++) {
-							if (currentPen === draggable._pens[i]) {
-								return true;
-							}
+			if (draggable._disabled) {
+				return false;
+			} else {
+				if (ghost !== undefined) {
+					ghost.remove();
+				}
+				draggable._parent.removeEventListener('mousemove', drag, true);
+				if (draggable._pens && draggable._pens.length > 0) {
+					var penned = false,
+						currentPen = draggable.element.parentNode,
+						isAPen = function (element) {
+							for (var i = 0; i <= draggable._pens.length - 1; i++) {
+								if (currentPen === draggable._pens[i]) {
+									return true;
+								}
+							};
 						};
-					};
-				for (var i = 0; i < draggable._pens.length - 1; i++) {
-					if (draggable._newX < (draggable._pens[i].offsetLeft + draggable._pens[i].offsetWidth) && draggable._newX > (draggable._pens[i].offsetLeft - draggable.element.offsetWidth) && draggable._newY > (draggable._pens[i].offsetTop - draggable.element.offsetHeight) && draggable._newY < (draggable._pens[i].offsetTop + draggable._pens[i].offsetHeight + draggable.element.offsetHeight)) {
-						penned = true;
-						draggable.element.style.position = '';
-						draggable._pens[i].appendChild(draggable.element);
-						break;
-					}
-				};
-				if (!penned) {
-					if (draggable._roam) {
-						document.querySelector('body').appendChild(draggable.element);
-					} else {
-						if (isAPen(currentPen)) {
-							currentPen.appendChild(draggable.element);
+					for (var i = 0; i < draggable._pens.length; i++) {
+						if (draggable._newX < (draggable._pens[i].offsetLeft + draggable._pens[i].offsetWidth) && draggable._newX > (draggable._pens[i].offsetLeft - draggable.element.offsetWidth) && draggable._newY > (draggable._pens[i].offsetTop - draggable.element.offsetHeight) && draggable._newY < (draggable._pens[i].offsetTop + draggable._pens[i].offsetHeight + draggable.element.offsetHeight)) {
+							penned = true;
 							draggable.element.style.position = '';
+							draggable._pens[i].appendChild(draggable.element);
+							break;
+						}
+					};
+					if (!penned) {
+						if (draggable._roam) {
+							document.querySelector('body').appendChild(draggable.element);
+						} else {
+							if (isAPen(currentPen)) {
+								currentPen.appendChild(draggable.element);
+								draggable.element.style.position = '';
+							}
 						}
 					}
 				}
 			}
 		},
 		startDrag = function (event) {
-			draggable._offY = event.clientY - parseInt(draggable.element.offsetTop);
-			draggable._offX = event.clientX - parseInt(draggable.element.offsetLeft);
-			draggable._boundsXR = (draggable._parent.offsetLeft + draggable._parent.offsetWidth) - draggable.element.offsetWidth;
-			draggable._boundsXL = draggable._parent.offsetLeft;
-			draggable._boundsXT = draggable._parent.offsetTop;
-			draggable._boundsXB = (draggable._parent.offsetTop + draggable._parent.offsetHeight) - draggable.element.offsetHeight;
-			if (draggable._ghosting) {
-				ghost = draggable.element.cloneNode();
-				draggable.element.parentNode.appendChild(ghost);
-				ghost.style.opacity = 0.2;
-				ghost.style.position = 'absolute';
-				ghost.style.left = draggable.element.offsetLeft + 'px';
-				ghost.style.top = draggable.element.offsetTop + 'px';
-			}	
-			draggable._parent.addEventListener('mousemove', drag, true);
+			if (draggable._disabled) {
+				return false;
+			} else {
+				draggable._offY = event.clientY - parseInt(draggable.element.offsetTop);
+				draggable._offX = event.clientX - parseInt(draggable.element.offsetLeft);
+				draggable._boundsXR = (draggable._parent.offsetLeft + draggable._parent.offsetWidth) - draggable.element.offsetWidth;
+				draggable._boundsXL = draggable._parent.offsetLeft;
+				draggable._boundsXT = draggable._parent.offsetTop;
+				draggable._boundsXB = (draggable._parent.offsetTop + draggable._parent.offsetHeight) - draggable.element.offsetHeight;
+				if (draggable._ghosting) {
+					ghost = draggable.element.cloneNode();
+					draggable.element.parentNode.appendChild(ghost);
+					ghost.style.opacity = 0.2;
+					ghost.style.position = 'absolute';
+					ghost.style.left = draggable.element.offsetLeft + 'px';
+					ghost.style.top = draggable.element.offsetTop + 'px';
+				}	
+				draggable._parent.addEventListener('mousemove', drag, true);
+			}
 		};
 	draggable.element.addEventListener('mousedown', startDrag, false);
     	draggable.element.addEventListener('mouseup', endDrag, false);
